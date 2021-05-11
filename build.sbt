2c2564ffbdb53a75c242be92d7f34895edd88871
Antonio Yuen <antonio.yuen@namely.com>
2021-05-11 16:20:09 -0400

    git actions and rename repo

---
+++ build.sbt
@@ -0,0 +1,31 @@
+test / parallelExecution := true
+
+lazy val root: Project = project
+  .in(file("."))
+  .aggregate(otel_tools, proto_test)
+  .enablePlugins(CommonSettings)
+  .enablePlugins(NoPublish)
+  .disablePlugins(Publish)
+  .settings(name := "scala-otel-tools")
+
+lazy val otel_tools: Project = project
+  .in(file("code/otel-tools"))
+  .dependsOn(proto_test % "test->compile")
+  .enablePlugins(BuildSettings)
+  .enablePlugins(Publish)
+  .disablePlugins(NoPublish)
+  .settings(name := "otel-tools")
+
+lazy val proto_test: Project = project
+  .in(file("code/.proto"))
+  .enablePlugins(BuildSettings)
+  .enablePlugins(NoPublish)
+  .disablePlugins(Publish)
+  .settings(
+    name := "protogen",
+      Compile / PB.protoSources ++= Seq(file("proto")),
+      Compile / PB.includePaths ++= Seq(file("proto/test")),
+      Compile / PB.targets := Seq(
+        scalapb.gen(flatPackage = false, javaConversions = false, grpc = true) -> (Compile / sourceManaged).value / "scalapb"
+      )
+  )
---
+++ project/CommonSettings.scala
@@ -0,0 +1,42 @@
+import Dependencies.Versions
+import org.scalafmt.sbt.ScalafmtPlugin.autoImport.scalafmtOnCompile
+import sbt.Keys._
+import sbt._
+
+object CommonSettings extends AutoPlugin {
+
+  override def trigger = allRequirements
+
+  override def globalSettings =
+    Seq(
+      scalaVersion := Versions.Scala213,
+      organization := "io.superflat",
+      organizationName := "Super Flat",
+      startYear := Some(2020),
+      organizationHomepage := Some(url("https://superflat.io/")),
+      homepage := Some(url("https://github.com/super-flat/scala-otel-tools")),
+      scmInfo := Some(ScmInfo(url("https://github.com/super-flat/scala-otel-tools"),
+        "git@github.com:super-flat/scala-otel-tools.git")),
+      licenses := List("Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
+      developers += Developer(
+        "contributors",
+        "Contributors",
+        "",
+        url("https://github.com/super-flat/scala-otel-tools/graphs/contributors")),
+      description := "scala-otel-tools - Scala shared code for telemetry.\n")
+
+  override def projectSettings =
+    Seq(
+      scalacOptions ++= Seq(
+        "-Xfatal-warnings",
+        "-deprecation",
+        "-Xlint"
+      ),
+      resolvers ++= Seq(Resolver.jcenterRepo, Resolver.sonatypeRepo("public"), Resolver.sonatypeRepo("snapshots")),
+      scalafmtOnCompile := true,
+      // show full stack traces and test case durations
+      Test / testOptions += Tests.Argument("-oDF"),
+      Test / logBuffered := false,
+      Test / fork := true
+    )
+}
