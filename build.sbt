c353180f6747b5ef062465f53d9c523607b9bab4
Antonio <32649897+AntonioYuen@users.noreply.github.com>
2021-05-24 12:59:54 -0400

    fix snapshot version and remove scalapb from compiled code (#18)

--- build.sbt
+++ build.sbt
@@ -1,10 +1,8 @@
-import scalapb.compiler.Version.scalapbVersion
-
 test / parallelExecution := true
 
 lazy val root: Project = project
   .in(file("."))
-  .aggregate(otel_tools, proto_test)
+  .aggregate(otel_tools)
   .enablePlugins(CommonSettings)
   .enablePlugins(NoPublish)
   .disablePlugins(Publish)
@@ -12,26 +10,7 @@ lazy val root: Project = project
 
 lazy val otel_tools: Project = project
   .in(file("code/otel-tools"))
-  .dependsOn(proto_test % "test->compile")
   .enablePlugins(BuildSettings)
   .enablePlugins(Publish)
   .disablePlugins(NoPublish)
   .settings(name := "otel-tools")
-
-lazy val proto_test: Project = project
-  .in(file("code/.proto"))
-  .enablePlugins(BuildSettings)
-  .enablePlugins(NoPublish)
-  .disablePlugins(Publish)
-  .settings(
-    libraryDependencies ++= Seq(
-      // scalapb
-      "com.thesamet.scalapb" %% "scalapb-runtime" % scalapbVersion % "protobuf"
-    ),
-    name := "protogen",
-      Compile / PB.protoSources ++= Seq(file("proto")),
-      Compile / PB.includePaths ++= Seq(file("proto/test")),
-      Compile / PB.targets := Seq(
-        scalapb.gen(flatPackage = false, javaConversions = false, grpc = true) -> (Compile / sourceManaged).value / "scalapb"
-      )
-  )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,4 @@
 import sbt.{Test, _}
-import scalapb.compiler.Version.{grpcJavaVersion, scalapbVersion}
 
 /**
  * Holds the list of dependencies used in the project and their various version
@@ -11,6 +10,9 @@ object Dependencies {
    * Versions number
    */
   object Versions {
+    val GrpcJavaVersion = "1.37.0"
+    val ScalaPbVersion = "0.11.3"
+
     val Scala213 = "2.13.5"
     val ScalaTestVersion = "3.2.9"
     val ScalaMockVersion = "5.1.0"
@@ -26,25 +28,30 @@ object Dependencies {
   val excludeGRPC = ExclusionRule(organization = "io.grpc")
 
   val Jars = Seq(
+    // gRPC
+    "io.grpc" % "grpc-stub" % GrpcJavaVersion % "provided",
     // Opentelemetry
     "io.opentelemetry" % "opentelemetry-api" % OpenTelemetryVersion,
     "io.opentelemetry" % "opentelemetry-sdk" % OpenTelemetryVersion,
     "io.opentelemetry.instrumentation" % "opentelemetry-grpc-1.5" % OpenTelemetryGRPCVersion,
     "io.opentelemetry" % "opentelemetry-extension-trace-propagators" % OpenTelemetryVersion,
     ("io.opentelemetry" % "opentelemetry-exporter-otlp-trace" % OpenTelemetryVersion).excludeAll(excludeGRPC),
-    ("io.opentelemetry" % "opentelemetry-exporter-otlp-metrics" % OpenTelemetryMetricsVersion).excludeAll(excludeGRPC),
-    "com.thesamet.scalapb" %% "scalapb-runtime-grpc" % scalapbVersion
+    ("io.opentelemetry" % "opentelemetry-exporter-otlp-metrics" % OpenTelemetryMetricsVersion).excludeAll(excludeGRPC)
   )
 
   /**
    * Test dependencies
    */
   val TestJars: Seq[ModuleID] = Seq(
+    // gRPC
+    "io.grpc" % "grpc-testing" % GrpcJavaVersion % Test,
+    // Protobuf
+    "com.thesamet.scalapb" %% "scalapb-runtime-grpc" % ScalaPbVersion % Test,
+    // Opentelemetry
+    "io.opentelemetry" % "opentelemetry-sdk-testing" % OpenTelemetryVersion % Test,
     // general
     "org.scalatest" %% "scalatest" % Versions.ScalaTestVersion % Test,
     "org.scalamock" %% "scalamock" % Versions.ScalaMockVersion % Test,
-    "io.grpc" % "grpc-testing" % grpcJavaVersion % Test,
-    "io.opentelemetry" % "opentelemetry-sdk-testing" % OpenTelemetryVersion % Test,
     "org.awaitility" % "awaitility-scala" % AwaitilityVersion % Test
   )
 }
