43f4d95dc4e40711de825a4bdbf132a0e6f989f8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-11-21 00:30:19 +0100

    Update awaitility-scala to 4.1.1 (#56)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
     val OpenTelemetryMetricsVersion: String = "1.6.0-alpha"
     val OpenTelemetryPrometheusVersion: String = "1.6.0-alpha"
 
-    val AwaitilityVersion: String = "4.1.0"
+    val AwaitilityVersion: String = "4.1.1"
   }
 
   import Dependencies.Versions._
